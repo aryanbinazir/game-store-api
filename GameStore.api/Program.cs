35a47b776172e9a9df58102de8032c80dd85f462
Aryan Binazir <binazir.kobe@gmail.com>
2025-11-08 18:17:50 +0330

    Add swagger

--- GameStore.api/Program.cs
+++ GameStore.api/Program.cs
@@ -4,11 +4,21 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddScoped<AppDbContext>();
 builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.MapGamesEndpoints();
 
